58f859d3aa90a9047e14be05b0f7ae9b61bd0618
Kyungbin Ryu <ryukbin94@gmail.com>
2020-05-10 18:39:14 +0900

    Test

---
+++ src/LoginScreen.js
@@ -0,0 +1,128 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+
+import FooterButton from './components/FooterButton';
+
+import Toast from 'react-native-easy-toast';
+
+import auth from '@react-native-firebase/auth';
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idText: '이메일',
+      pwText: '비밀번호',
+      id: '',
+      pw: '',
+      loading: false,
+    };
+  }
+
+  handleLogin = () => {
+    const { id, pw } = this.state;
+    this.setState({ loading: true });
+    auth().signInWithEmailAndPassword(id, pw)
+    .then(() => {
+      this.setState({ loading: false });
+      this.props.navigation.navigate('Main');
+    })
+    .catch(() => {
+      this.setState({ loading: false });
+      this.refs.toast.show('잘못된 로그인 정보입니다. 다시 로그인해 주세요!', 500);
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./icon.png')}
+          style={styles.icon}
+          />
+          <Text style={styles.welcomeText}>환영합니다</Text>
+          <TextInput
+            style={styles.textInputButton}
+            onChangeText={(id) => this.setState({id})}
+            placeholder={this.state.idText}
+            autoCorrect={false}
+          />
+          <TextInput
+            style={styles.textInputButton}
+            onChangeText={(pw) => this.setState({pw})}
+            placeholder={this.state.pwText}
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
+          {
+            this.state.loading
+            ? <ActivityIndicator style={styles.loginButtonStyle} size="small" />
+            : <FooterButton
+                buttonText="로그인"
+                style={styles.loginButtonStyle}
+                onPress={this.handleLogin}
+              />
+          }
+          <Text style={styles.noAccountText}>계정이 없으신가요?</Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate('SignUp')}
+          >
+            <Text style={styles.makeAccountText}>계정 만들기</Text>
+          </TouchableOpacity>
+          <Toast ref="toast" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D8D8D8',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    marginBottom: 70,
+  },
+  welcomeText: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 23,
+  },
+  textInputButton: {
+    width: 288,
+    borderColor: 'gray',
+    paddingVertical: 10,
+    borderWidth: 0.3,
+    paddingHorizontal: 5,
+    borderRadius: 2,
+    backgroundColor: 'white',
+    height: 50,
+  },
+  loginButtonStyle : {
+    width: 315,
+    height: 50,
+    marginTop: 50,
+  },
+  noAccountText: {
+    marginTop: 30,
+    fontSize: 12,
+    color: '#5B5A5A',
+  },
+  makeAccountText: {
+    fontSize: 12,
+    color: '#9013FE',
+  },
+});
---
+++ src/SettingScreen.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+import FooterButton from './components/FooterButton';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+import auth from '@react-native-firebase/auth';
+
+import Toast from 'react-native-easy-toast';
+
+export default class SettingScreen extends Component {
+
+  handleSignOut = () => {
+    auth().signOut()
+    .then(() => this.props.navigation.navigate('Home'))
+      .catch(() =>
+        this.refs.toast.show('오류가 발생했습니다. 다시 시도해 주세요', 1000),
+      );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.arrow}
+          onPress={() => this.props.navigation.navigate('Main')}
+        >
+          <AntDesign name="arrowleft" size={30} color="#5B5A5A" />
+        </TouchableOpacity>
+        <Image
+          source={require('./icon.png')}
+          style={styles.icon}
+        />
+        <Text style={styles.currentVersionText}>현재 버전 1.0.0</Text>
+        <Text style={styles.currentVersionText}>지원환경 iOS 7.0 이상</Text>
+        <FooterButton
+          buttonText= '로그아웃'
+          style={styles.FooterButton}
+          onPress={this.handleSignOut}
+        />
+        <Text style={styles.currentVersionText}> © 원래는 웹을 하려고 했었다. </Text>
+        <Toast ref="toast" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D8D8D8',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+  },
+  currentVersionText: {
+    marginVertical: 15,
+    fontSize: 12,
+    color: '#5B5A5A',
+  },
+  FooterButton: {
+    marginTop: 250,
+  },
+  arrow: {
+    position: 'absolute',
+    left: 20,
+    top: 50,
+  },
+});
